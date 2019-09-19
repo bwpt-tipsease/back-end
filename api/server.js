0b27c0aeba49d351b9f09863179368fc91e4d650
heuzin <matheusqs.arq@gmail.com>
2019-09-19 19:33:34 -0400

    server.js file created

---
+++ api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+require('dotenv').config();
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+module.exports = server;
