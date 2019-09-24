adcbb2b1524ddd0b46fd4c392eaae2239a26d7bc
heuzin <matheusqs.arq@gmail.com>
2019-09-23 20:37:25 -0400

    git modules

--- knexfile.js
+++ knexfile.js
@@ -20,4 +20,21 @@ module.exports = {
       directory: './database/seeds'
     }
   },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'knex_migrations',
+    },
+    seeds: {
+      directory: './database/seeds/production',
+    },
+    useNullAsDefault: true,
+  },
 };
